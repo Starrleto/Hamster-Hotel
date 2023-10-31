2da1ef0c1f4c599da93b218c74dd0ecf5f6a5837
Starrleto <ellierasuli@gmail.com>
2023-10-31 11:04:34 -0700

    Update one

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
-//alert("I work");
+//alert("Hai");
+
+let aString = "Pee";
+let anInt = 4;
+let aBool = true;
+
+const dontChange = "Soup";
+
+console.log(`Hello ${dontChange}`);
+
+let anArray = [1, "two"];
+
+console.log(anArray);
+
+let element = document.getElementById("element");
